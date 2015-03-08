4f4424e5774c98398749247964bab715047e12c1
Peter LoVerso <rdsqc22@gmail.com>
2015-03-08 01:04:05 -0500

    initial commit of all files

---
+++ README.md
@@ -0,0 +1,2 @@
+This program is designed to take one .axt file containing multiple chromosomes, and separate out those data into individual files by chromosome.
+Usage: perl axtToChrs.pl <in.axt> <outdir>
---
+++ axtToChrs.pl
@@ -0,0 +1,13 @@
+#!/usr/bin/perl
+
+# Usage: axtToChrs.pl $inFile $outDir
+# inFile: The input .axt file
+# $outDir: The directory into which to write the chromosome axt file.
+
+# Example usage:
+# perl axtToChrs.pl myAxtFile.axt ./
+
+use warnings;
+use strict;
+
+my ($locs, $dataFolder, $retWindow, $outDir) = @ARGV;
